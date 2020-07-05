0c283b638f374d7382207164a637583a4380abc4
Lauren Valkyrie <lauren.valkyrie@gmail.com>
2020-07-05 21:20:56 +0000

    end of lesson commit

--- change_file_type.sh
+++ change_file_type.sh
@@ -1,10 +1,16 @@
 #! /usr/bin/env bash
+
+
 # rename a bunch of files in a directory to have a .txt extension 
 # mv <source file> <destination file> is the command for changing a file name 
 
-
+set -u 
 
 for file_item in $1/*; do 
+# for file_item in ""/bin /boot /dev ...; do 
 mv -v $file_item $file_item.$2
+# mv -v /bin /bin."" 
 done
  
+
+# principle of least surprise: in designing a program given two equally valid choices of how to build something, build the one that is least surprising to the user.
